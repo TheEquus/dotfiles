16c44d49ba4ffd5e6b310cfdaa523d790125c886
Equus <nieanniee@gmail.com>
2023-07-02 17:46:44 +1000

    added toggle between floating panes

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -77,6 +77,7 @@ keys = [
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], "f", lazy.window.toggle_floating(), desc="Toggle between floating"),
 
     Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
@@ -183,12 +184,12 @@ screens = [
 
 # Drag floating layouts.
 # mouse = [
-#     Drag([mod], "Button1", lazy.window.set_position_floating(),
-#          start=lazy.window.get_position()),
+#	Drag([mod], "Button1", lazy.window.set_position_floating(),
+#        start=lazy.window.get_position()),
 #     Drag([mod], "Button3", lazy.window.set_size_floating(),
 #          start=lazy.window.get_size()),
 #     Click([mod], "Button2", lazy.window.bring_to_front())
-# ]
+#]
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
